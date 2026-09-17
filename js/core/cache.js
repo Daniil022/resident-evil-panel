83ac2f5d3bae4ae17d94c2b2a85a4ef0203dd85a
Daniil022 <158138582+Daniil022@users.noreply.github.com>
2026-09-17 21:36:09 +0600

    Update cache.js

--- js/core/cache.js
+++ js/core/cache.js
@@ -17,7 +17,7 @@ export function cacheSet(key, value) {
 
 export function cacheInvalidate(key) {
   if (key) store.delete(key);
-  else store.clear();ы
+  else store.clear();
 }
 
 export function cacheInvalidatePrefix(prefix) {
